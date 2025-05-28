e33ada48aea13722b634e5715a07801f2aca0138
Marc Bouchez <theparaplux@gmail.com>
2025-05-28 21:10:06 +0200

    test url prod

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,13 @@
+const isProduction = window.location.origin.includes("partysnap.uggyboogie.fr");
+
 export const config = {
     pocketbase: {
-        url: "http://localhost:8092",
-        avatarUrl: "http://localhost:8092/api/files/users",
-        photosUrl: "http://localhost:8092/api/files/photos",
+        url: isProduction ? "https://api.partysnap.uggyboogie.fr" : "http://localhost:8092",
+        avatarUrl: isProduction
+            ? "https://api.partysnap.uggyboogie.fr/api/files/users"
+            : "http://localhost:8092/api/files/users",
+        photosUrl: isProduction
+            ? "https://api.partysnap.uggyboogie.fr/api/files/photos"
+            : "http://localhost:8092/api/files/photos",
     },
 };
